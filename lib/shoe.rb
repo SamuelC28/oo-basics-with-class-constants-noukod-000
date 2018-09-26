a5a6a38038a16a26cc5d928d0e3a0ab04a98fe06
Samuel Cornet <samuel@noukod.com>
2018-09-26 13:17:54 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,17 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+   BRANDS = []
   def initialize(brand)
     @brand = brand
   end
+
+ def brand=(brand)
+   if @brand.include?(brand)
+   else
+   BRANDS << brand
+ end
+ end
 
   def cobble
     self.condition = "new"
